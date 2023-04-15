f4657ed3e4b6d6c8c31860d4d3528c0d0af49a7d
aidan.newell <acnewell09@gmail.com>
2023-04-15 10:35:57 -0500

    created a transaction form for home page

--- my-money/src/pages/home/Home.js
+++ my-money/src/pages/home/Home.js
@@ -1,11 +1,14 @@
 // styles
 import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>transaction list</div>
-      <div className={styles.sidebar}></div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
   );
 }
